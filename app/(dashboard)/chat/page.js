eea70ea77bcbf227f1a43a6590d717896b411a6d
Fauzan Shidqi <fauzanshidqi21@gmail.com>
2024-03-29 21:07:30 +0700

    himChat Ai

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -4,10 +4,11 @@ import { dehydrate, HydrationBoundary,QueryClient } from '@tanstack/react-query'
 
 const ChatPage = () => {
     const queryClient = new QueryClient()
-    return 
-    <HydrationBoundary state={dehydrate(queryClient)}>
-        <Chat/>
-    </HydrationBoundary>
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Chat/>
+        </HydrationBoundary>
+    )
 }
 
 export default ChatPage
